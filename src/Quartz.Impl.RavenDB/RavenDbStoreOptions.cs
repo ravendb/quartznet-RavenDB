74a67aaa6e2f564039123fa3f7ffc7a63b78479d
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-08-13 23:04:58 +0200

    Added UseRavenDbStore method

---
+++ src/Quartz.Impl.RavenDB/RavenDbStoreOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Quartz.Impl.RavenDB
+{
+    public class RavenDbStoreOptions : PropertiesHolder
+    {
+        protected internal RavenDbStoreOptions(SchedulerBuilder parent) : base(parent)
+        {
+        }
+    }
+}
---
+++ src/Quartz.Impl.RavenDB/SchedulerBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Quartz.Util;
+
+namespace Quartz.Impl.RavenDB
+{
+    public static class SchedulerBuilderExtensions
+    {
+        public static SchedulerBuilder UseRavenDbStore(this SchedulerBuilder builder, Action<RavenDbStoreOptions>? options = null)
+        {
+            builder.SetProperty(StdSchedulerFactory.PropertyJobStoreType,
+                typeof(RavenJobStore).AssemblyQualifiedNameWithoutVersion());
+            options?.Invoke(new RavenDbStoreOptions(builder));
+            return builder;
+        }
+    }
+}
