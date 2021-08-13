990fa54ff3b1cc7eab51ccb8f6f3e3f8c4d1152f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-08-14 00:07:43 +0200

    Added docs to UseRavenDbStore

--- src/Quartz.Impl.RavenDB/IPropertyConfigurerExtensions.cs
+++ src/Quartz.Impl.RavenDB/IPropertyConfigurerExtensions.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using Quartz.Spi;
 using Quartz.Util;
 
 namespace Quartz.Impl.RavenDB
 {
     public static class IPropertyConfigurerExtensions
     {
+        /// <summary>
+        ///     Use <see cref="RavenJobStore"/> as persistent <see cref="IJobStore"/>.
+        /// </summary>
         public static IPropertyConfigurer UseRavenDbStore(this IPropertyConfigurer builder, Action<RavenDbStoreOptions>? options = null)
         {
             builder.SetProperty(StdSchedulerFactory.PropertyJobStoreType,
