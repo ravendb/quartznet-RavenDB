7ae3e2e3e8688db6a86d95b229194c4f2d59fa7a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-08-14 02:21:54 +0200

    Ported example jobs and triggers over to core project

--- src/Quartz.Impl.RavenDB.WorkerExample/Worker.cs
+++ src/Quartz.Impl.RavenDB.WorkerExample/Worker.cs
@@ -10,23 +10,76 @@ namespace Quartz.Impl.RavenDB.WorkerExample
     {
         private readonly ILogger<Worker> _logger;
 
-        private readonly IScheduler _scheduler;
+        private readonly ISchedulerFactory _sf;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, ISchedulerFactory sf)
         {
             _logger = logger;
+            _sf = sf;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
+            var scheduler = await _sf.GetScheduler(stoppingToken);
 
+            var emptyFridgeJob = JobBuilder.Create<EmptyFridge>()
+                .WithIdentity("EmptyFridgeJob", "Office")
+                .RequestRecovery()
+                .Build();
 
-                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
-            }
+            var turnOffLightsJob = JobBuilder.Create<TurnOffLights>()
+                .WithIdentity("TurnOffLightsJob", "Office")
+                .RequestRecovery()
+                .Build();
+
+            var checkAliveJob = JobBuilder.Create<CheckAlive>()
+                .WithIdentity("CheckAliveJob", "Office")
+                .RequestRecovery()
+                .Build();
+
+            var visitJob = JobBuilder.Create<Visit>()
+                .WithIdentity("VisitJob", "Office")
+                .RequestRecovery()
+                .Build();
+
+            // Weekly, Friday at 10 AM (Cron Trigger)
+            var emptyFridgeTrigger = TriggerBuilder.Create()
+                .WithIdentity("EmptyFridge", "Office")
+                .WithCronSchedule("0 0 10 ? * FRI")
+                .ForJob("EmptyFridgeJob", "Office")
+                .Build();
+
+            // Daily at 6 PM (Daily Interval Trigger)
+            var turnOffLightsTrigger = TriggerBuilder.Create()
+                .WithIdentity("TurnOffLights", "Office")
+                .WithDailyTimeIntervalSchedule(s => s
+                    .WithIntervalInHours(24)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(18, 0)))
+                .Build();
+
+            // Periodic check every 10 seconds (Simple Trigger)
+            var checkAliveTrigger = TriggerBuilder.Create()
+                .WithIdentity("CheckAlive", "Office")
+                .StartAt(DateTime.UtcNow.AddSeconds(3))
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(10)
+                    .RepeatForever())
+                .Build();
+
+            var visitTrigger = TriggerBuilder.Create()
+                .WithIdentity("Visit", "Office")
+                .StartAt(DateTime.UtcNow.AddSeconds(3))
+                .Build();
+            
+            await scheduler.ScheduleJob(checkAliveJob, checkAliveTrigger, stoppingToken);
+            await scheduler.ScheduleJob(emptyFridgeJob, emptyFridgeTrigger, stoppingToken);
+            await scheduler.ScheduleJob(turnOffLightsJob, turnOffLightsTrigger, stoppingToken);
+            await scheduler.ScheduleJob(visitJob, visitTrigger, stoppingToken);
+
+            while (!stoppingToken.IsCancellationRequested) await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
         }
     }
 }
